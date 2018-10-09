a97c8355d9f651726c2841f49ec4058b1cf5e758
Tyler Conn <tyler.conn@hotmail.com>
2018-10-08 21:21:11 -0400

    Updated to use the static ip

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://beeradmin:password@18.218.168.204:27017/dummyDB'
+	url: 'mongodb://beeradmin:password@18.224.236.196:27017/dummyDB'
 }
